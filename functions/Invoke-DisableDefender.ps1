214e2527a18dba442c91cf6d75cccc6df2fe18d0
ManuelTest <manueltesting01@gmail.com>
2025-10-30 16:29:45 +0100

    Remove redundant check for existing AtlasModules directory in Invoke-TelemetryDisable function

--- functions/Invoke-DisableDefender.ps1
+++ functions/Invoke-DisableDefender.ps1
@@ -1,10 +1,7 @@
 function Invoke-DisableDefender {
 
 	Invoke-WebRequest -Uri "https://github.com/ManuelBiscotti/test/raw/refs/heads/main/tools/AtlasModules.zip" -OutFile "$env:TEMP\AtlasModules.zip"
-	Expand-Archive -Path "$env:TEMP\AtlasModules.zip" -DestinationPath "$env:TEMP" -Force -ErrorAction SilentlyContinue   
-	if (Test-Path "C:\Windows\AtlasModules") {
-    	Remove-Item "C:\Windows\AtlasModules" -Recurse -Force -ErrorAction SilentlyContinue
-	}
+	Expand-Archive -Path "$env:TEMP\AtlasModules.zip" -DestinationPath "$env:TEMP" -Force -ErrorAction SilentlyContinue
 	Move-Item -Path (Join-Path $env:TEMP 'AtlasModules') -Destination 'C:\Windows' -Force -ErrorAction SilentlyContinue
     Start-Process -FilePath 'C:\Windows\AtlasModules\DisableDefender.cmd' -NoNewWindow -Wait
 
--- myscript.ps1
+++ myscript.ps1
@@ -253,9 +253,6 @@ function Invoke-TelemetryDisable {
 	# Add NoTelemetry package (ATLAS)
 	Invoke-WebRequest -Uri "https://github.com/ManuelBiscotti/test/raw/refs/heads/main/tools/AtlasModules.zip" -OutFile "$env:TEMP\AtlasModules.zip"
 	Expand-Archive -Path "$env:TEMP\AtlasModules.zip" -DestinationPath "$env:TEMP" -Force -ErrorAction SilentlyContinue   
-	if (Test-Path "C:\Windows\AtlasModules") {
-    	Remove-Item "C:\Windows\AtlasModules" -Recurse -Force -ErrorAction SilentlyContinue
-	}
 	Move-Item -Path (Join-Path $env:TEMP 'AtlasModules') -Destination 'C:\Windows' -Force -ErrorAction SilentlyContinue
 	Start-Process -FilePath 'C:\Windows\AtlasModules\DisableTelemetry.cmd' -NoNewWindow -Wait
 
